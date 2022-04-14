256fdbbee5feaf1311866807dc39ecf159186262
impostor <77080832+impostor5875@users.noreply.github.com>
2022-04-14 09:32:09 -0400

    no antispam???????????????????

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2666,20 +2666,20 @@ class PlayState extends MusicBeatState
 					}
 				}
 
-				// Kill extremely late notes and cause misses
-				if (Conductor.songPosition > noteKillOffset + daNote.strumTime)
-				{
-					if (daNote.mustPress && !cpuControlled &&!daNote.ignoreNote && !endingSong && (daNote.tooLate || !daNote.wasGoodHit)) {
-						noteMiss(daNote);
-					}
+				// Kill extremely late notes and cause misses (antispam or normal misses??)
+				//if (Conductor.songPosition > noteKillOffset + daNote.strumTime)
+				//{
+					//if (daNote.mustPress && !cpuControlled &&!daNote.ignoreNote && !endingSong && (daNote.tooLate || !daNote.wasGoodHit)) {
+						//noteMiss(daNote);
+					//}
 
-					daNote.active = false;
-					daNote.visible = false;
+					//daNote.active = false;
+					//daNote.visible = false;
 
-					daNote.kill();
-					notes.remove(daNote, true);
-					daNote.destroy();
-				}
+					//daNote.kill();
+					//notes.remove(daNote, true);
+					//daNote.destroy();
+				//}
 			});
 		}
 		checkEventNote();
